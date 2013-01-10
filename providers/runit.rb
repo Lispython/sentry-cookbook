4edc4b232f8ed89373c72f0ce1315c2df98336ab
Alex <Alexandr.S.rus@gmail.com>
2013-01-10 18:13:59 +0400

    Updated runit instance provider

--- providers/runit.rb
+++ providers/runit.rb
@@ -1,4 +1,22 @@
 action :init do
-  Chef::Log.info("Make instance via runit")
-  puts("hello world")
+  Chef::Log.info("Make instance via runit #{new_resource.name}")
+
+  sentry_new_resource = new_resource
+
+  log_folder = sentry_new_resource.log_folder || "/var/log/runit/#{sentry_new_resource.name}/"
+
+  runit_service sentry_new_resource.name do
+    template_name sentry_new_resource.name
+    run_restart false
+    options :user => sentry_new_resource.user,
+    :group => sentry_new_resource.group,
+    :service_name => sentry_new_resource.name,
+    :config => sentry_new_resource.config,
+    :pidfile => sentry_new_resource.pidfile,
+    :port => sentry_new_resource.port,
+    :host => sentry_new_resource.host,
+    :virtualenv => sentry_new_resource.virtualenv
+    :log_folder => log_folder
+  end
+
 end
