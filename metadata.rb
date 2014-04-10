3fa3ac9a5b8b150fc95a6b8238b9104e62b1e952
Alex <Alexandr.S.rus@gmail.com>
2014-04-10 22:00:59 +0400

    Updated version.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "alex@obout.ru"
 license          "BSD, see LICENSE for more details."
 description      "Install/Configures sentry application"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.6"
 
 depends "python"
